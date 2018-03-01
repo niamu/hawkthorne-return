8986b368164aed915722dd8fe57092a1ff4d7931
Brendon Walsh <brendonwalsh@niamu.com>
2018-03-01 00:54:21 -0500

    [WIP] TileMap collision layer

--- src/hawkthorne/client.cljs
+++ src/hawkthorne/client.cljs
@@ -1,6 +1,5 @@
 (ns hawkthorne.client
   (:require [cljsjs.phaser-ce]
-            [tmx2edn.core :refer [tmx->edn]]
             [hawkthorne.atlas :refer [character->atlas]]
             [hawkthorne.game :as game]
             [hawkthorne.util :as util]))
@@ -15,25 +14,25 @@
                         "images/characters/abed/base.png"
                         nil
                         (clj->js (character->atlas :abed))))
-  (.. game -load (binary :hallway
-                         "maps/hallway.tmx"
-                         (fn [k data]
-                           (->> data
-                                util/arraybuffer->str
-                                tmx->edn))))
+  (game/preload-tilemap! game "maps/hallway.tmx")
   (prn :preload))
 
 (defn create
   [game]
-  (prn :cache (.. game -cache
-                  (getKeys (-> js/Phaser .-Cache .-BINARY))))
-  #_(prn :hallway (clj->js (.. game -cache (getBinary :hallway))))
+  (.. game -physics (startSystem (-> js/Phaser .-Physics .-P2JS)))
+  (game/create-tilemap! game "maps/hallway.tmx")
   (let [player (.. game -add (sprite 100 0 :abed))
         walk (.. player -animations
                  (add "walk-left"
                       (.. js/Phaser -Animation
                           (generateFrameNames "walk-left-" 0 3))))]
-    (.. player -animations (play "walk-left" 10 true)))
+    (.. player -animations (play "walk-left" 10 true))
+    (-> game .-physics .-p2 (.enable player))
+    (.. game -camera (follow player))
+    (aset (-> game .-physics .-p2 .-gravity) "y" 250)
+    #_(aset (-> player .-body .-bounce) "y" 0.2)
+    #_(aset (-> player .-body) "damping" 1)
+    #_(aset (-> player .-body) "collideWorldBounds" true))
   (prn :create))
 
 (aset js/window "onload"
--- src/hawkthorne/game.cljs
+++ src/hawkthorne/game.cljs
@@ -1,7 +1,59 @@
 (ns hawkthorne.game
   ;; Phaser convenience functions
   (:require [cljsjs.phaser-ce]
-            [hawkthorne.state :as state]))
+            [hawkthorne.state :as state]
+            [hawkthorne.util :as util]
+            [tmx2edn.core :refer [tmx->edn]]))
+
+(defn preload-tilesets!
+  [game tilesets]
+  (doall (map (fn [tileset]
+                (.. game -load (image (:name tileset) (:image tileset))))
+              tilesets)))
+
+(defn preload-tilemap!
+  [game tilemap]
+  (.. game -load (binary tilemap tilemap
+                         (fn [k data]
+                           (let [map-edn (->> (util/arraybuffer->str data)
+                                              tmx->edn)]
+                             (preload-tilesets! game (:tilesets map-edn))
+                             (.. game -load (tilemap tilemap nil
+                                                     (clj->js map-edn)
+                                                     (-> js/Phaser .-Tilemap
+                                                         .-TILED_JSON)))
+                             map-edn)))))
+
+(defn create-tileset-images!
+  [game tilemap level]
+  (doall (map (fn [tileset]
+                (.addTilesetImage level (:name tileset)))
+              (:tilesets (.. game -cache (getBinary tilemap))))))
+
+(defn create-layers!
+  [game tilemap level]
+  (doall (map (fn [layer]
+                (let [l (.createLayer level (:name layer))]
+                  (when (= "collision" (:name layer))
+                    (.resizeWorld l)
+                    (.setCollision level
+                                   (->> (:data layer)
+                                        (map-indexed (fn [idx d] [idx d]))
+                                        (filter (fn [m]
+                                                  (not (zero? (second m)))))
+                                        keys)
+                                   true (:name layer))
+                    (-> game .-physics .-p2 (.convertTilemap level l
+                                                             true true)))))
+              (filter #(= :tilelayer (:type %))
+                      (:layers (.. game -cache (getBinary tilemap)))))))
+
+(defn create-tilemap!
+  [game tilemap]
+  (let [level (-> game .-add (.tilemap tilemap))]
+    (create-tileset-images! game tilemap level)
+    (create-layers! game tilemap level)
+    (.. game -physics -p2 (setBoundsToWorld true true true true true))))
 
 (defn build-state
   "Pass game object to each state function"
